a24502b0c00d9ec2beac5717d51d7dc4cce1dca0
Antonio Santiago Dueñas <antonio.duenas@dowjones.com>
2018-05-02 20:58:16 -0500

    add first component

--- cinta-roja-rules/src/App.js
+++ cinta-roja-rules/src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import MyFirstComponent from './MyFirstComponent';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <MyFirstComponent text={"Holi Devf"} />
+          <MyFirstComponent text={"nerjnreu"} />
+          <MyFirstComponent text={"vnjdfnvfejvner"} />
+          <MyFirstComponent text={"Holi vcfnvef"} />
       </div>
     );
   }
